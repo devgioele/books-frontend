e89e4636555309b870bc0b544de8fa531a2ec968
Gioele.DeVitti <gdevitti@unibz.it>
2021-05-10 18:02:40 +0200

    Separate currency from price

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -72,10 +72,7 @@ export default function BookCard(props) {
       </Grid>
       <Grid item>
         <Typography variant="body1">
-          <b>
-            {book.currency}
-            {book.price}
-          </b>
+          <b>{`${book.currency} ${book.price}`}</b>
         </Typography>
       </Grid>
     </Grid>
