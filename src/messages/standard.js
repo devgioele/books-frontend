65ec4e6a5841e38156005582feaa1e77aeccdab3
Gioele De Vitti <gioeledevittidev@gmail.com>
2021-06-10 22:45:31 +0200

    Clean up messages and enums

--- src/messages/standard.js
+++ src/messages/standard.js
@@ -9,8 +9,9 @@ const StdMessages = {
     `The communication with the server failed${
       operation === '' ? '' : ` while ${operation}`
     }. Check your internet connection.`,
-  IMPORT_ERROR: (filename, reason = undefined) =>
-    `Could not import file '${filename}'!${reason ? ` ${reason}` : ''}`,
+  MAX_NUMBER_IMAGES: () => 'Maximum number of images reached.',
+  INVALID_FORMAT: () => 'Invalid file format.',
+  UNSUPPORTED_FORMAT: () => 'Unsupported file format.',
   SIGNUP_DONE: () => 'Signed up! You are ready to go!',
   CURRENCIES_ERROR: () => `The list of supported currencies could not be loaded. 
   ${StdMessages.CONTACT_SUPPORT()}`,
