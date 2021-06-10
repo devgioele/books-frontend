7a49d15e7317f3f46dea6eb2e275c04e4ee53b05
Gioele De Vitti <gioeledevittidev@gmail.com>
2021-06-10 20:28:19 +0200

    Prettify book cover without image

--- src/components/SellBooksList/SellBooksList.jsx
+++ src/components/SellBooksList/SellBooksList.jsx
@@ -35,11 +35,19 @@ const useStyles = makeStyles((theme) => ({
   bookCover: {
     [theme.breakpoints.up('xs')]: {
       width: '100px',
+      minWidth: '100px',
+      maxWidth: '100px',
       height: '150px',
+      minHeight: '150px',
+      maxHeight: '150px',
     },
     [theme.breakpoints.up('md')]: {
       width: '120px',
+      minWidth: '120px',
+      maxWidth: '120px',
       height: '180px',
+      minHeight: '180px',
+      maxHeight: '180px',
     },
   },
   sellButton: {
